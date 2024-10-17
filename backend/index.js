6a7dbf5613359887a17abcca2123d0b55c9ac1ee
Nzagwaneza <anzagwaneza@gmail.com>
2024-10-17 08:35:19 +0200

    Added auth routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB } from "./DB/connectDB.js";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello world 125!");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
