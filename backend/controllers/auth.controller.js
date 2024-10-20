490e43eea849acbda7b98e263c3c8a12b14457cf
Nzagwaneza <anzagwaneza@gmail.com>
2024-10-21 01:05:04 +0200

    Added Auth endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -189,3 +189,17 @@ export const resetPassword = async (req, res) => {
     res.status(400).json({ success: false, message: error.message });
   }
 };
+
+export const checkAuth = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password"); //password will not be selected. In this case when we console log user there will not password displayed( =undefined)
+    if (!user) {
+      res.status(400).json({ success: false, message: "User not found" });
+    }
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error in checkAuth");
+    return res.status(400).json({ success: false, message: error.message });
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { connectDB } from "./DB/connectDB.js";
 import dotenv from "dotenv";
+
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
@@ -9,6 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser()); //for parsing incoming cookies
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
