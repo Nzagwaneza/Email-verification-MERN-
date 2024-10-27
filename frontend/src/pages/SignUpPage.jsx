bd9aeb29e91b9c89a7159b852e7e78aadebd4ddf
Nzagwaneza <anzagwaneza@gmail.com>
2024-10-27 07:40:32 +0200

    Added signup UI

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { Lock, Mail, User } from "lucide-react";
+import { useState } from "react";
+import Input from "../components/input";
+import { Link } from "react-router-dom";
+import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = () => {
+    console.log("");
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-green-700-opacity-80 bg-opacity-50 backdrop-filter 
+      backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8 bg-blue-300 opacity-9">
+        <h2 className="text-3xl font-bold mb-6 text-center ">Create Account</h2>
+        <form onSubmit={handleSignUp}>
+          <Input
+            icon={User}
+            type="text"
+            placeholder="Full name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            icon={Mail}
+            type="email"
+            placeholder="Email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            icon={Lock}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <PasswordStrengthMeter password={password} />
+
+          <motion.button
+            className="mt-5 py-3 px-4 w-full flex justify-center bg-gradient-to-r 
+          from-slate-500 to-blue-400 text-white font-bold rounded-lg shadow-lg 
+          hover:from-green-800 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 
+          focus:ring-offset-2 focus:ring-offset-slate-500 transition duration-200"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+          >
+            Sign Up
+          </motion.button>
+        </form>
+      </div>
+      <div className="px-8 py-4 bg-blue-600 bg-opacity-50 flex justify-center">
+        <p className="text-base text-white">
+          Already have an account?{" "}
+          <Link to={"/login"} className="text-black hover:underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
 };
 
 export default SignUpPage;
