f211cf9e573e0fab6a9d2ec7723852a50246cdae
Nzagwaneza <anzagwaneza@gmail.com>
2024-10-27 21:38:53 +0200

    Email verification UI

---
+++ frontend/src/pages/EmailVerificationPage.jsx
@@ -0,0 +1,115 @@
+import { motion } from "framer-motion";
+import { Mail, Lock, Loader } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../components/input";
+import { useEffect, useRef, useState } from "react";
+
+const EmailVerificationPage = () => {
+  const [code, setCode] = useState(["", "", "", "", "", ""]);
+  const inputRefs = useRef([]);
+  const navigate = useNavigate();
+  const isLoading = false;
+
+  const handleChange = (index, value) => {
+    const newCode = [...code];
+
+    //handle pasted content
+    if (value.length > 1) {
+      const pastedCode = value.slice(0, 6).split("");
+      for (let i = 0; i < 6; i++) {
+        newCode[i] = pastedCode[i] || "";
+      }
+      setCode(newCode);
+      //focus on the last non-empty input or the first one
+      const lastFilledIndex = newCode.findLastIndex((digit) => digit !== "");
+      const focusIndex = lastFilledIndex < 5 ? lastFilledIndex + 1 : 5;
+      inputRefs.current[focusIndex].focus();
+    } else {
+      newCode[index] = value;
+      setCode(newCode);
+
+      //Move focus to the next input field if value is entered
+      if (value && index < 5) {
+        inputRefs.current[index + 1].focus();
+      }
+    }
+  };
+  const handleKeyDown = (index, e) => {
+    if (e.key === "Backspace" && !code[index] && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const verificationCode = code.join("");
+    console.log(`Verification code submitted:${verificationCode}`);
+  };
+
+  //Auto submit when all fields are filled
+  useEffect(() => {
+    if (code.every((digit) => digit !== "")) {
+      handleSubmit(new Event("submit"));
+    }
+  }, [code]);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-green-700-opacity-80 bg-opacity-50 backdrop-filter 
+      backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8 bg-blue-300 opacity-9">
+        <h2 className="text-3xl font-bold mb-6 text-center ">
+          Verify Your Email
+        </h2>
+        <p>Enter the 6-digit code sent to your email address.</p>
+        <form className="space-y-6 mt-2">
+          <div className="flex justify-between">
+            {code.map((digit, index) => (
+              <input
+                key={index}
+                ref={(el) => (inputRefs.current[index] = el)}
+                type="text"
+                maxLength="6"
+                value={digit}
+                onChange={(e) => handleChange(index, e.target.value)}
+                onKeyDown={(e) => handleKeyDown(index, e)}
+                className="w-12 h-12 text-center text-2xl font-bold bg-blue-200  border-2 border-gray-500 rounded-lg focus:border-blue-500 focus:outline-none "
+              />
+            ))}
+          </div>
+          <motion.button
+            onSubmit={handleSubmit}
+            className="mt-5 py-3 px-4 w-full flex justify-center bg-gradient-to-r 
+          from-slate-500 to-blue-400 text-white font-bold rounded-lg shadow-lg 
+          hover:from-green-800 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 
+          focus:ring-offset-2 focus:ring-offset-slate-500 transition duration-200"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <Loader className="w-6 h-6 animate-spin" />
+            ) : (
+              "Verify Email"
+            )}
+          </motion.button>
+        </form>
+      </div>
+      <div className="px-8 py-4 bg-blue-600 bg-opacity-50 flex justify-center">
+        <p className="text-base text-white">
+          Don't get the code?{" "}
+          <Link to={"/toBeAddedLater"} className="text-black hover:underline">
+            Resend Code
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default EmailVerificationPage;
