f91c3c36e407c7f5b20dc5b25510fedd173db35b
Nzagwaneza <anzagwaneza@gmail.com>
2024-10-31 07:59:34 +0200

    Completed dashboard and implemented  logout

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { useAuthStore } from "../store/authStore";
+import { formatDate } from "../Utils/utils";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  const { user, logout } = useAuthStore();
+  const handleLogout = (e) => {
+    e.preventDefault();
+
+    logout();
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 1, scale: 0.9 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full mx-auto mt-10 p-8 bg-slate-400 bg-opacity-80 backdrop-filter backdrop-blur-lg rounded-xl shadow-2xl border-blue-800"
+    >
+      <h2 className="text-3xl font-bold mb-6 text-center text-white bg-gradient-to-r from-slate-600 bg-gray-950">
+        Dashboard
+      </h2>
+      <div className="space-y-6">
+        <motion.div
+          className="p-4 bg-slate-400 bg-opacity-50 rounded-lg border border-gray-700"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <h3 className="text-xl font-semibold text-blue-950 mb-3">
+            Profile Information
+          </h3>
+          <p className="text-blue-950">Name: {user.name}</p>
+          <p className="text-blue-950">Email: {user.email}</p>
+        </motion.div>
+        <motion.div
+          className="p-4 bg-slate-400 bg-opacity-50 rounded-lg border border-gray-700"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <h3 className="text-xl font-semibold text-blue-950 mb-3">
+            Account Activity
+          </h3>
+          <p className="text-blue-950">
+            <span className="font-bold">Joined: </span>
+            {new Date(user.createdAt).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
+          <p className="text-blue-950">
+            <span className="font-bold">Last Login: </span>
+            {formatDate(user.lastLogin)}
+          </p>
+        </motion.div>
+      </div>
+      <motion.button
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        onClick={handleLogout}
+        className="mt-5 py-3 px-4 w-full flex justify-center bg-gradient-to-r 
+          from-slate-500 to-blue-700 text-white font-bold rounded-lg shadow-lg 
+          hover:from-grayS-800 hover:to-blue-950 focus:outline-none focus:ring-2 focus:ring-blue-500 
+          focus:ring-offset-2 focus:ring-offset-slate-500 transition duration-200"
+      >
+        Logout
+      </motion.button>
+    </motion.div>
+  );
 };
 
 export default DashboardPage;
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -56,6 +56,22 @@ export const useAuthStore = create((set) => ({
     }
   },
 
+  logout: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      await axios.post(`${API_URL}/logout`);
+      set({
+        user: null,
+        isAuthenticated: false,
+        error: null,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({ error: "Error in logging out", isLoading: false });
+      throw error;
+    }
+  },
+
   verifyEmail: async (code) => {
     set({ isLoading: true, error: null });
     try {
